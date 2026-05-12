b6db3a8a4a512879e5877a5c57e2fd0c179d69f5
nicolasedward-a11y <nicolas.edward@upe.br>
2026-05-12 15:44:36 -0300

    Update two_sum.py

--- src/two_sum.py
+++ src/two_sum.py
@@ -2,4 +2,14 @@ from src.my_array import MyArray
 
 
 def two_sum(array: MyArray, target: int) -> tuple[int, int]:
-    raise NotImplementedError
+    seen = {}
+    
+    for i in range(len(array)):
+        complement = target - array[i]
+        
+        if complement in seen:
+            return (seen[complement],i)
+        
+        seen[array[i]] =i
+    
+    return (-1, -1)
