8b9743a3094687ca8fb2f4d8a83b3c4645d73d59
nicolasedward-a11y <nicolas.edward@upe.br>
2026-05-12 15:41:11 -0300

    Update kth_to_last.py

--- src/kth_to_last.py
+++ src/kth_to_last.py
@@ -1,5 +1,19 @@
 from src.my_node import MyNode
 
-
 def kth_to_last(head: MyNode, k: int) -> int:
-    raise NotImplementedError
+    if head is None or k <= 0:  # k=0 inválido
+        return -1
+
+    slow = head
+    fast = head
+
+    for _ in range(k - 1):  # ← k-1 em vez de k
+        fast = fast.next
+        if fast is None:
+            return -1
+
+    while fast.next:  # ← fast.next em vez de fast
+        slow = slow.next
+        fast = fast.next
+
+    return slow.value  
