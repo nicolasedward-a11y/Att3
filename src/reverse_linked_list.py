abbbac645be6ae37b7aad60eea6823ccc47907bb
nicolasedward-a11y <nicolas.edward@upe.br>
2026-05-12 15:44:15 -0300

    Update reverse_linked_list.py

--- src/reverse_linked_list.py
+++ src/reverse_linked_list.py
@@ -2,4 +2,15 @@ from src.my_node import MyNode
 
 
 def reverse_linked_list(head: MyNode) -> MyNode:
-    raise NotImplementedError
+    prev = None
+    current = head
+
+    while current:
+        next_node = current.next
+        current.next = prev
+
+        prev = current
+        current = next_node
+
+    return prev
+    
