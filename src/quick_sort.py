f582ea735c7b4061abf19c7654c55e7a76b1d22d
nicolasedward-a11y <nicolas.edward@upe.br>
2026-05-12 15:43:26 -0300

    Update quick_sort.py

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -2,4 +2,37 @@ from src.my_array import MyArray
 
 
 def quick_sort(array: MyArray) -> MyArray:
-    raise NotImplementedError
+    if len(array) <= 1:
+        return array
+
+    pivot = array[len(array) - 1]
+
+    left = MyArray()
+    equal = MyArray()
+    right = MyArray()
+
+    for i in range(len(array)):
+        if array[i] < pivot:
+            left.append(array[i])
+
+        elif array[i] > pivot:
+            right.append(array[i])
+
+        else:
+            equal.append(array[i])
+
+    left = quick_sort(left)
+    right = quick_sort(right)
+
+    result = MyArray()
+
+    for i in range(len(left)):
+        result.append(left[i])
+
+    for i in range(len(equal)):
+        result.append(equal[i])
+
+    for i in range(len(right)):
+        result.append(right[i])
+
+    return result
