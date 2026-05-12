911185b682ac10664ba0c157eeb48b20b61c0963
nicolasedward-a11y <nicolas.edward@upe.br>
2026-05-12 15:43:43 -0300

    Update remove_duplicates.py

--- src/remove_duplicates.py
+++ src/remove_duplicates.py
@@ -2,4 +2,21 @@ from src.my_node import MyNode
 
 
 def remove_duplicates(head: MyNode) -> MyNode:
-    raise NotImplementedError
+    if head is None:
+        return None
+
+    seen = set()
+
+    current = head
+    prev = None
+
+    while current:
+        if current.value in seen:
+            prev.next = current.next
+        else:
+            seen.add(current.value)
+            prev = current
+
+        current = current.next
+
+    return head
