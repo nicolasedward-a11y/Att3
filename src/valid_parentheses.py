2a861d80eb4bca9a63a629639fe6a4d4eb041e42
nicolasedward-a11y <nicolas.edward@upe.br>
2026-05-12 15:44:53 -0300

    Update valid_parentheses.py

--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -1,5 +1,23 @@
-# from src.my_stack import MyStack
+from src.my_stack import MyStack
 
 
 def is_valid_parentheses(string: str) -> bool:
-    raise NotImplementedError
+    stack = MyStack()
+
+    pairs = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+
+    for char in string:
+        if char in '([{':
+            stack.push(char)
+
+        elif char in ')]}':
+            top = stack.pop()
+
+            if top != pairs[char]:
+                return False
+
+    return stack.is_empty()
