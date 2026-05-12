66cae849ffd63fc401f5c4948d579719a4384ae0
nicolasedward-a11y <nicolas.edward@upe.br>
2026-05-12 15:44:00 -0300

    Update reverse_array.py

--- src/reverse_array.py
+++ src/reverse_array.py
@@ -2,4 +2,15 @@ from src.my_array import MyArray
 
 
 def reverse_array(array: MyArray) -> MyArray:
-    raise NotImplementedError
+    left = 0
+    right = len(array) - 1
+
+    while left < right:
+        temp = array[left]
+        array[left] = array[right]
+        array[right] = temp
+
+        left += 1
+        right -= 1
+
+    return array
