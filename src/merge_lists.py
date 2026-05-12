856042826124688a12864ba36ca533634082bf3b
nicolasedward-a11y <nicolas.edward@upe.br>
2026-05-12 15:41:28 -0300

    Update merge_lists.py

--- src/merge_lists.py
+++ src/merge_lists.py
@@ -2,4 +2,17 @@ from src.my_node import MyNode
 
 
 def merge_lists(lista1: MyNode, lista2: MyNode) -> MyNode:
-    raise NotImplementedError
+    if lista1 is None:
+        return lista2
+
+    if lista2 is None:
+        return lista1
+
+    if lista1.value < lista2.value:
+        lista1.next = merge_lists(lista1.next, lista2)
+        return lista1
+    else:
+        lista2.next = merge_lists(lista1, lista2.next)
+        return lista2
+    
+        
